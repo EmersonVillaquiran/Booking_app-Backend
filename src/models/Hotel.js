667c4191490c77643bc5b4dd51162890976abd5a
CamiloVillaquiran <112994996+EmersonVillaquiran@users.noreply.github.com>
2024-05-31 21:50:46 -0500

    change hotel by string x text

--- src/models/Hotel.js
+++ src/models/Hotel.js
@@ -7,7 +7,7 @@ const Hotel = sequelize.define('hotel', {
         allowNull: false
     },
     description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
     },
     price: {
